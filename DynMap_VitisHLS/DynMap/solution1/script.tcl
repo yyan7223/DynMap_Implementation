d1eed4a05f0b56ad19b3756cda0e96d07ac5fd6d
yyan7223 <22B305016@stu.hit.edu.cn>
2024-01-04 11:01:01 +0800

    Signed-off-by: yyan7223 <22B305016@stu.hit.edu.cn> RTL exported

--- DynMap_VitisHLS/DynMap/solution1/script.tcl
+++ DynMap_VitisHLS/DynMap/solution1/script.tcl
@@ -5,14 +5,15 @@
 ############################################################
 open_project DynMap
 set_top runOne
-add_files DynMap/DynMap_4HLS.cpp
 add_files DynMap/DynMap_4HLS.hpp
-add_files -tb DynMap/main.cpp
-add_files -tb DynMap/result.golden.dat
+add_files DynMap/DynMap_4HLS.cpp
+add_files -tb DynMap/result.golden.dat -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb DynMap/main.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 open_solution "solution1" -flow_target vivado
-set_part {xc7z035ffg676-2}
+set_part {xc7z035-ffg676-2}
 create_clock -period 10 -name default
-#source "./DynMap/solution1/directives.tcl"
+config_export -format ip_catalog -rtl verilog
+source "./DynMap/solution1/directives.tcl"
 csim_design
 csynth_design
 cosim_design
