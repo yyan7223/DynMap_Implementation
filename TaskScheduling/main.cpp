25e4f99ba0b1093459ea48c22d7096c094e19b6d
yyan7223 <22B305016@stu.hit.edu.cn>
2024-05-24 20:33:24 +0800

    Signed-off-by: yyan7223 <22B305016@stu.hit.edu.cn>

--- TaskScheduling/main.cpp
+++ TaskScheduling/main.cpp
@@ -1,7 +1,7 @@
 #include"utils.hpp"
 
 void updateActualTime(){
-    actualTime += timeStride;
+    actualTime += timeQuantum;
 }
 
 void updateWeightSum(){
@@ -13,32 +13,102 @@ void updateWeightSum(){
 }
 
 void updateVirtualTime(){
-    virtualTime += timeStride / W;
+    virtualTime += timeQuantum / W;
 }
 
-void selectOneTaskToRun(){
+void balanceVirtualTime(float lag){
+    virtualTime += lag / W;
+}
+
+void updateTaskDestroyStatus(){
+    for(vector<Task>::iterator iter = activeTaskQueue.begin(); iter != activeTaskQueue.end(); iter++){      
+        if(iter->dt <= actualTime){ 
+            lags.push_back(virtualTime - iter->ve); // record the lag of the destroyed task
+            activeTaskQueue.erase(iter); // remove the destroyed task from activeTaskQueue
+            iter--;
+        }
+    }
+    for(int i = 0; i < lags.size(); i++){ // if there are destroyed tasks
+        for(auto& activeTask : activeTaskQueue){ 
+            activeTask.balanceVE(lags[i], W); // refresh the ve of each activetask according to the lag of each destroyed task
+        }
+        balanceVirtualTime(lags[i]);
+    }
+}
+
+int selectMutilpleTasksToRun(int tasksNum){
     // firstly find eligible tasks
-    eligibleTaskID.clear();
+    eligibleTaskQueueIdx.clear();
     for(int i = 0; i < activeTaskQueue.size(); i++){ 
-        if(activeTaskQueue[i].eligible) eligibleTaskID.push_back(i);
+        if(activeTaskQueue[i].eligible) eligibleTaskQueueIdx.push_back(i);
     }
 
-    // select one of the eligible tasks to runs
+    // select one of the eligible tasks to run
     int minIdx = 0;
-    if(eligibleTaskID.size() <= 1){ // if only one or less eligible task, default select the first task to run
-        activeTaskQueue[eligibleTaskID[minIdx]].startRunning(); 
+    if(eligibleTaskQueueIdx.size() <= 1){ // if only one or less eligible task, default select the first task to run
+        activeTaskQueue[eligibleTaskQueueIdx[minIdx]].startRunning(timeQuantum); 
     }
     else{ // if several tasks are eligible
         float minVD = 9999.0f;
-        for(int i = 0; i < eligibleTaskID.size(); i++){ // choose the one with the minimum vd to run
-            if(activeTaskQueue[eligibleTaskID[i]].vd < minVD){
-                minVD = activeTaskQueue[eligibleTaskID[i]].vd;
+        for(int i = 0; i < eligibleTaskQueueIdx.size(); i++){ // choose the one with the minimum vd to run
+            if(activeTaskQueue[eligibleTaskQueueIdx[i]].vd < minVD){
+                minVD = activeTaskQueue[eligibleTaskQueueIdx[i]].vd;
                 minIdx = i;
             }
         }
-        if(actualTime == 1) minIdx = 1;
-        activeTaskQueue[eligibleTaskID[minIdx]].startRunning(); 
+        if(actualTime == 1) minIdx = 1; // a hardcoded task choice inidcated by paper as eligible and deadline of both tasks are same
+        for(int i = 0; i < tasksNum; i++){
+            activeTaskQueue[eligibleTaskQueueIdx[minIdx + i]].startRunning(timeQuantum); 
+        }
     }
+    cout<<"Time: "<<actualTime<<"s, running task ID: "<<activeTaskQueue[eligibleTaskQueueIdx[minIdx]].ID<<endl; 
+    return minIdx;
+}
+
+void selectMutilpleTasksToRun_sort(int tasksNum){
+    eligibleTaskQueueIdx.clear();
+    vector<int> ineligibleTaskQueueIdx; 
+    for(int i = 0; i < activeTaskQueue.size(); i++){ // firstly select eligible tasks
+        if(activeTaskQueue[i].eligible) eligibleTaskQueueIdx.push_back(i);
+        else ineligibleTaskQueueIdx.push_back(i);
+    }
+
+    if(eligibleTaskQueueIdx.size() < tasksNum){ // if insufficient as indicated by taskNum, supplement tasks with minimum vd
+        int queueLen = ineligibleTaskQueueIdx.size();
+        for (int i = 0; i < queueLen - 1; i++) { // bubble sort vd from small to large, can be optimized to rb-tree   
+            for (int j = 0; j < queueLen - 1 - i; j++) { 
+                if (activeTaskQueue[ineligibleTaskQueueIdx[j]].vd > activeTaskQueue[ineligibleTaskQueueIdx[j+1]].vd) {
+                    swap(ineligibleTaskQueueIdx[j], ineligibleTaskQueueIdx[j+1]);
+            }
+        }
+        int diff = tasksNum - eligibleTaskQueueIdx.size();
+        for(int i = 0; i < diff; i++) eligibleTaskQueueIdx.push_back(ineligibleTaskQueueIdx[i]); // supplement insufficient tasks
+    }
+    }
+    else if(eligibleTaskQueueIdx.size() > tasksNum){ // if more than indicated by taskNum, remove tasks with maximum vd
+        int queueLen = eligibleTaskQueueIdx.size();
+        for (int i = 0; i < queueLen - 1; i++) { // bubble sort vd from small to large, can be optimized to rb-tree   
+            for (int j = 0; j < queueLen - 1 - i; j++) { 
+                if (activeTaskQueue[eligibleTaskQueueIdx[j]].vd > activeTaskQueue[eligibleTaskQueueIdx[j+1]].vd) {
+                    swap(eligibleTaskQueueIdx[j], eligibleTaskQueueIdx[j+1]);
+                }
+            }
+        }
+        int diff = eligibleTaskQueueIdx.size() - tasksNum;
+        for(int i = 0; i < diff; i++) eligibleTaskQueueIdx.pop_back(); // remove redundant tasks
+    }
+    else {};
+
+    // run the selected tasks
+    for(int i = 0; i < tasksNum; i++){
+        activeTaskQueue[eligibleTaskQueueIdx[i]].startRunning(timeQuantum); 
+    }
+    cout<<"Time: "<<actualTime<<"s, running task ID:";
+    for (int i = 0; i < eligibleTaskQueueIdx.size(); i++)
+    {
+        cout<<" "<<activeTaskQueue[eligibleTaskQueueIdx[i]].ID<<","; 
+    }
+    cout<<endl;
 }
 
 void updateTasksAttr(){
@@ -46,8 +116,10 @@ void updateTasksAttr(){
     for(auto& activeTask : activeTaskQueue){ // & is for updating the value
         activeTask.updateVE(virtualTime);
         activeTask.updateVD();
-        activeTask.updateEligible(virtualTime);
-        activeTask.resetReceivedServiceTime();
+    }
+    updateTaskDestroyStatus(); // update vt and ve if there are tasks destoryed, 
+    for(auto& activeTask : activeTaskQueue){ // & is for updating the value
+        activeTask.updateEligible(virtualTime); // update eligible of each activeTask according to latest vt and ve
     }
 }
 
@@ -55,7 +127,7 @@ void updateTimeStamp(){
     updateActualTime();
     updateVirtualTime();
     for(auto& activeTask : activeTaskQueue){ // & is for updating the value 
-        activeTask.stopRunning(timeStride); // update the running time of the running task
+        activeTask.stopRunning(timeQuantum); // task can only run for one timestride for each request
     }
 }
 
@@ -69,15 +141,31 @@ void updateActiveTaskQueue(){
 
 int main(){
 
-    // manually creat some tasks, Task(int id, float weight, float requestLength, float spawnTime)
-    Task task1(1, 2.0f, 2.0f, 0.0f); taskQueue.push_back(task1);
-    Task task2(2, 2.0f, 1.0f, 1.0f); taskQueue.push_back(task2);
+    // manually creat some tasks, Task(int id, float weight, float requestLength, float spawnTime, float destroyTime)
+    // Task task1(1, 2.0f, 2.0f, 0.0f, 999.0f); taskQueue.push_back(task1);
+    // Task task2(2, 2.0f, 1.0f, 1.0f, 999.0f); taskQueue.push_back(task2);
+
+    Task task1(1, 2.0f, 2.56f, 0.0f, 999.0f); taskQueue.push_back(task1);
+    Task task2(2, 2.0f, 1.38f, 0.0f, 999.0f); taskQueue.push_back(task2);
+    Task task3(3, 2.0f, 3.66f, 0.0f, 999.0f); taskQueue.push_back(task3);
+    Task task4(4, 2.0f, 4.87f, 0.0f, 999.0f); taskQueue.push_back(task4);
+    Task task5(5, 2.0f, 5.23f, 0.0f, 999.0f); taskQueue.push_back(task5);
+    Task task6(6, 2.0f, 3.39f, 0.0f, 999.0f); taskQueue.push_back(task6);
+    Task task7(7, 2.0f, 0.88f, 0.0f, 999.0f); taskQueue.push_back(task7);
+    Task task8(8, 2.0f, 9.23f, 0.0f, 999.0f); taskQueue.push_back(task8);
 
     while(true){ // one iteration represents a new second
         // At the beginning of each second
         updateActiveTaskQueue(); // check whether spawn new tasks and add to activeTaskQueue
         updateTasksAttr(); // update vd ve of waiting tasks
-        selectOneTaskToRun(); // choose one task to run
+
+        // during each second
+        // Todo: check pingpong operations complete, and decides number of tasks to run
+        int tasksNum = 3; 
+        bool pingpongDone = true;
+        selectMutilpleTasksToRun_sort(tasksNum); // choose tasks to run
+
+        // at the end of each second
         updateTimeStamp(); // move to next second
     };
     return 0;
--- TaskScheduling/utils.hpp
+++ TaskScheduling/utils.hpp
@@ -15,11 +15,12 @@ class Task{
         float u; // variable from paper, task received service time
         bool eligible; // variable from paper, task is eligible
         float st; // task spawn time
+        float dt; // task destroy time
         float leftTime; // the left request time
         bool isRunning; // indicate whether the task is running
         bool isFirstReq; // indicate whether is the first request
         Task(){};
-        Task(int id, float weight, float requestLength, float spawnTime){
+        Task(int id, float weight, float requestLength, float spawnTime, float destroyTime){
             ID = id;
             ve = 0.0f;
             vd = 0.0f;
@@ -28,44 +29,56 @@ class Task{
             u = 0.0f;
             eligible = false;
             st = spawnTime;
+            dt = destroyTime;
             leftTime = 0.0f;
             isRunning = false;
             isFirstReq = true;
         };
     public:
-        void updateVE(float virtualTime){
-            if(isFirstReq){
-                ve = virtualTime;
-                isFirstReq = false;
+        void updateVE(float vt){
+            if(leftTime <= 0){ // VE updating is only allowed when finish executing the request length
+                if(isFirstReq){
+                    ve = vt;
+                    isFirstReq = false;
+                }
+                else{
+                    ve = ve + u / w;
+                    u = 0.0f; // reset u to 0 after updating VE
+                }
             }
-            else{
-                ve = ve + u / w;
-            }
+        }
+        void balanceVE(float lag, float W){
+            ve = ve + (1 - w) * lag / W;
         }
         void updateVD(){
-            vd = ve + r / w;
+            if(leftTime <= 0){ // VE updating is only allowed when finish executing the request length
+                vd = ve + r / w;
+            }
         }
-        void updateEligible(float virtualTime){
-            if(virtualTime >= ve) eligible = true;
+        void updateEligible(float vt){
+            if(vt >= ve) eligible = true;
             else eligible = false;
         }
-        void startRunning(){
+        void startRunning(float quantum){
             isRunning = true;
+            if(leftTime <= 0.0f){
+                leftTime = r;
+            }
+            leftTime -= quantum;
         }
-        void resetReceivedServiceTime(){
-            u = 0.0f; // reset u to 0 after updating VE
-        }
-        void stopRunning(float stride){
-            if(isRunning) u += stride; // only update the received service time when the task actually run in this time stride
+        void stopRunning(float quantum){
+            if(isRunning) u += quantum; // only update the received service time when the task actually run in this time quantum
+            if(u > r) u = r; // actual received service time can only be smaller or equal to request time
             isRunning = false;
         }
 };
 
 float actualTime = 0.0f;
 float virtualTime = 0.0f;
-float timeStride = 1.0f; // 1 second
-vector<Task> taskQueue;
-vector<Task> activeTaskQueue;
-vector<int> eligibleTaskID;
+float timeQuantum = 1.0f; // 1 second
+vector<Task> taskQueue; // all possible tasks luanched by users
+vector<Task> activeTaskQueue; // user actually launched tasks
+vector<float> lags; // the lag of each deconstructed tasks
+vector<int> eligibleTaskQueueIdx; 
 float W = 0.0f; // weightSum
 
